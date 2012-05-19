c08cb7b6e0d7b8aee1b4371603aa9846457cf082
Nils Domrose <nils@appseleration.com>
2012-05-19 23:20:46 +0200

    * added basic opendcm templates

--- urls.py
+++ urls.py
@@ -14,6 +14,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     
+    # index
+    (r'^$', 'opendcm.views.index'),
+    (r'^system/(?P<name>[-\w]+)/$', 'opendcm.views.detail'),
+    
+    
     # user auth includes
     (r'^auth/', include('userauth.urls')),
 )
